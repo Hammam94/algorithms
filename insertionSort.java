c43f8b1df4d3ef05187172d47610a5cf67b4e907
Hammam94 <gentle_engineer@hotmail.com>
2016-04-29 12:37:42 -0700

    implementation

--- insertionSort.java
+++ insertionSort.java
@@ -1,4 +1,16 @@
 
 public class insertionSort {
-
+	public static void Sort(int[] arr){
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j >= 0; j--) {
+				if(arr[j] < arr[i]) {
+					arr[i] += arr[j];
+					arr[j] = arr[i] - arr[j];
+					arr[i] -= arr[j];
+				}else{
+					break;
+				}
+			}
+		}
+	}
 }
